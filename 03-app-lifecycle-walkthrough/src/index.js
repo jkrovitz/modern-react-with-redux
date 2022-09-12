746b58ebcc1afe5639e73a606f94dec6dca7d18a
Jeremy Krovitz CW <jkrovitz.cw@mmm.com>
2022-09-12 13:02:17 -0500

    Add 03-app-lifecycle-walkthrough folder

---
+++ 03-app-lifecycle-walkthrough/src/index.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+
+        // THIS IS THE ONLY TIME we do direct assignment
+        // to this.state
+        this.state = { lat: null };
+
+        window.navigator.geolocation.getCurrentPosition(
+            position => {
+                // We called setState!!!
+                this.setState({ lat: position.coords.latitude });
+
+            },
+            err => console.log(err)
+        );
+    }
+    
+    // React says we have to define render!!
+    render() {
+
+        return <div>Latitude: {this.state.lat} </div>
+    }
+}
+
+ReactDOM.render(
+    <App />,
+    document.querySelector('#root')
+)
